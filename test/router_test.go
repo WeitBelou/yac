b54394b261ce54406adc36cd90fcf732054a267d
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-09-28 00:13:32 +0300

    Move concurrent tests to separate file.

--- test/router_test.go
+++ test/router_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -90,68 +89,4 @@ func TestRouterMethodNotAllowed(t *testing.T) {
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusMethodNotAllowed, w.Code)
 	assert.JSONEq(t, `{"status": 405, "error": "method 'POST' not allowed on path '/'"}`, w.Body.String())
-}
-
-func TestRouterConcurrentUse(t *testing.T) {
-	router := yac.Router{}
-
-	cases := []struct {
-		path   string
-		method string
-	}{
-		{"/", http.MethodGet},
-		{"/", http.MethodPost},
-		{"/users", http.MethodGet},
-		{"/users", http.MethodPost},
-	}
-
-	for _, c := range cases {
-		err := router.Handle(c.method, c.path, echoHandlerFunc)
-		require.Nil(t, err, "can not init router")
-	}
-
-	n := 1000
-	wg := sync.WaitGroup{}
-	wg.Add(n)
-
-	for i := 0; i < n; i++ {
-		go func() {
-			defer wg.Done()
-
-			for _, c := range cases {
-				w := httptest.NewRecorder()
-				req := httptest.NewRequest(c.method, c.path, nil)
-
-				router.ServeHTTP(w, req)
-				assert.Equal(t, http.StatusOK, w.Code, "failed to respond")
-				assert.Equal(t, fmt.Sprintf("%s:%s", req.Method, req.URL.Path), w.Body.String())
-			}
-		}()
-	}
-
-	wg.Wait()
-}
-
-func TestRouterConcurrentInit(t *testing.T) {
-	path := "/"
-	method := http.MethodGet
-	handler := emptyHandlerFunc
-
-	router := yac.Router{}
-	router.Handle(method, path, handler)
-
-	n := 1000
-
-	wg := sync.WaitGroup{}
-	wg.Add(n)
-
-	for i := 0; i < n; i++ {
-		go func() {
-			defer wg.Done()
-			err := router.Handle(method, path, handler)
-			assert.NotNil(t, err, "can init router twice")
-		}()
-
-	}
-	wg.Wait()
 }
